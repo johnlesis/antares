892f243ebb6b553d8f5d5cf4dff5e751c1ce7b9f
Johnlesis <johnlesis91@gmail.com>
2026-04-17 19:06:18 +0300

    remove: removed skeleton app components

--- config/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Controllers\PatientController;
-
-return [
-    ['GET',  '/config-test',      PatientController::class, 'list',   200],
-    ['GET',  '/config-test/{id}', PatientController::class, 'show',   200],
-    ['POST', '/config-test',      PatientController::class, 'create', 201],
-];
